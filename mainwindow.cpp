1e95d973a80e2251627be2a3c7bbaab73b3c47cd
Nathan <sysmetryxgosu@gmail.com>
2017-11-15 17:49:00 +0100

    PARTIE 2 : Gestion d'un code
    Le code choisi ici est 1980

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,7 +3,7 @@ ________________________________________________________________________________
 |
 |       EEEEEE       sSSSS  MM       MM     EEEEEE                      LAPORTE Nathan 2Z2                                                              EEEEEE       sSSSS  MM       MM     EEEEEE
 |       EE         sS       MMMM   MMMM     EE                          laporte_n@esme.fr                           NOVEMBRE 2017                       EE         sS       MMMM   MMMM     EE
-|       EEEEE       sSS     MM  MM   MM     EEEEEE                      https://github.com/Sysmetryx/               PARTIE 1                            EEEEE       sSS     MM  MM   MM     EEEEEE
+|       EEEEE       sSS     MM  MM   MM     EEEEEE                      https://github.com/Sysmetryx/               PARTIE 2                            EEEEE       sSS     MM  MM   MM     EEEEEE
 |       EE            Ss    MM       MM     EE                                                                      GUI                                 EE            Ss    MM       MM     EE
 |       EEEEE    SSSSs      MM       MM     EEEEEE                                                                  IHM sous Qt                         EEEEE    SSSSs      MM       MM     EEEEEE                                                                                                                                                                                                                              
 |_____________________________________________________________________________________________________________________________________________________________________________________________________________________
@@ -33,7 +33,7 @@ MainWindow::MainWindow(QWidget *parent)
 
     }
     m_pButtonO = new QPushButton("Ouvrir !", this);
-    connect (m_pButtonO, &QPushButton::clicked, this, &MainWindow::OpenButton);
+    connect (m_pButtonO, &QPushButton::clicked, this, &MainWindow::OnClickedPushButton);
     mainLayout->addWidget(m_pButtonO, 3, 0, 1, 5);
     this->setCentralWidget (new QWidget(this));
     this->centralWidget()->setLayout(mainLayout);
@@ -42,12 +42,62 @@ MainWindow::MainWindow(QWidget *parent)
 void MainWindow::OnClickedPushButton()
 {
     QPushButton *clickedButton = qobject_cast<QPushButton *>(sender());
-    QMessageBox::about(this, "message", "You pushed the button " + clickedButton->text());
+    //QMessageBox::about(this, "message", "You pushed the button " + clickedButton->text());
+    if(clickedButton->text() != "Ouvrir !")
+    {
+        bool ok = false;
+        int nombre = clickedButton->text().toInt(&ok, 10);
+        validator(nombre);
+        if(isvalid())
+            m_pButtonO->setStyleSheet("background-color: green;");
+        if (!isvalid())
+            m_pButtonO->setStyleSheet("background-color: red;");
+
+    }
+    else
+    {
+        if(isvalid())
+            QMessageBox::about(this, "Message", "Code juste !");
+        else
+            QMessageBox::about(this, "Message", "Code faux !");
+    }
+}
+
+bool MainWindow::isvalid()
+{
+    int i;
+    bool juste = true;
+    for(i = 0; i < 4; i++)
+    {
+        if(val[i] == false)
+            juste = false;
+    }
+    return juste;
+}
+
+void MainWindow::validator(int n)
+{
+    int i = 0;
+    bool juste = false;
+    for(i = 0; i < 4; i++)
+    {
+        if(n == code[i])
+        {
+            val[i] = true;
+            juste = true;
+            break;
+        }
+    }
+    if(juste == false)
+    {
+        for(i = 0; i < 4; i++)
+            val[i] = false;
+    }
 }
 
 void MainWindow::OpenButton()
 {
-    QMessageBox::about(this, "message", "Vous avez essayé d'Ouvrir !");
+   // QMessageBox::about(this, "message", "Vous avez essayé d'Ouvrir !");
 }
 
 
--- mainwindow.h
+++ mainwindow.h
@@ -3,7 +3,7 @@ ________________________________________________________________________________
 |
 |       EEEEEE       sSSSS  MM       MM     EEEEEE                      LAPORTE Nathan 2Z2                                                              EEEEEE       sSSSS  MM       MM     EEEEEE
 |       EE         sS       MMMM   MMMM     EE                          laporte_n@esme.fr                           NOVEMBRE 2017                       EE         sS       MMMM   MMMM     EE
-|       EEEEE       sSS     MM  MM   MM     EEEEEE                      https://github.com/Sysmetryx/               PARTIE 1                            EEEEE       sSS     MM  MM   MM     EEEEEE
+|       EEEEE       sSS     MM  MM   MM     EEEEEE                      https://github.com/Sysmetryx/               PARTIE 2                            EEEEE       sSS     MM  MM   MM     EEEEEE
 |       EE            Ss    MM       MM     EE                                                                      GUI                                 EE            Ss    MM       MM     EE
 |       EEEEE    SSSSs      MM       MM     EEEEEE                                                                  IHM sous Qt                         EEEEE    SSSSs      MM       MM     EEEEEE                                                                                                                                                                                                                              
 |_____________________________________________________________________________________________________________________________________________________________________________________________________________________
@@ -26,12 +26,13 @@ public:
     void OnClickedPushButton();
     void OpenButton();
     void validator(int n);
+    bool isvalid();
     ~MainWindow();
 private:
     QLabel *m_pLabel;
     QPushButton *m_pButton[BUTTON_NBR];
     QPushButton *m_pButtonO;
-    int const code [4] = {1, 9, 8, 1};
+    int const code[4] = {1, 9, 8, 0};
     bool val[4] = {false, false, false, false};
 };
 
